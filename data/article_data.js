e30cc9e4c3480d8adcaf56f607d997b299d3ec32
Bible <bearsk711@gmail.com>
2023-04-27 20:58:21 +0900

    Make article database

---
+++ data/article_data.js
@@ -0,0 +1,19 @@
+const articles = [
+  { title: 'React에서 상태관리하기', url: 'https://mingule.tistory.com/74' },
+  {
+    title: '남들이 사용해서 Redux 인가, 필요에 의해 쓰는 Redux 인가.',
+    url: 'https://velog.io/@mingsomm/%EB%A6%AC%EC%95%A1%ED%8A%B8%EC%9D%98-%EC%83%81%ED%83%9C%EA%B4%80%EB%A6%AC-redux-%ED%8E%B8',
+  },
+  {
+    title: '웹 프론트엔드 개발자, 어떻게 준비해야 할까?',
+    url: 'https://medium.com/codesquad-kr/%EC%9B%B9-%ED%94%84%EB%A1%A0%ED%8A%B8%EC%97%94%EB%93%9C-%EA%B0%9C%EB%B0%9C%EC%9E%90-%EC%96%B4%EB%96%BB%EA%B2%8C-%EC%A4%80%EB%B9%84%ED%95%B4%EC%95%BC-%ED%95%A0%EA%B9%8C-5ac7bb6ff2a9',
+  },
+  {
+    title: '일 잘하는 프론트엔드 개발자의 기술',
+    url: 'https://ordinary-code.tistory.com/134',
+  },
+  {
+    title: '컴퓨팅 사고와 개발 실력 늘리는 공부법',
+    url: 'https://yozm.wishket.com/magazine/detail/1950/',
+  },
+];
