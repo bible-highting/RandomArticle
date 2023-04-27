53f221ba28b12186d2c6c0a4415c48917c370220
Bible <bearsk711@gmail.com>
2023-04-27 22:39:01 +0900

    Add readFlag on each article data
    - isRead flag를 통해 읽은 기사에 체크하기 기능 추가
    - 단 데이터베이스자체를 바꾸는게 아니어서 새로고침하면 처음으로 돌아감. 데이터베이스 연결시 수정할 것

--- popup/script.js
+++ popup/script.js
@@ -1,10 +1,25 @@
 const btn = document.querySelector('.randomBtn');
 const articleTitle = document.querySelector('.articleTitle');
+const readFlag = document.querySelector('.readFlag');
+let randomIndex;
 
 const onClickMovetoRandomPage = () => {
-  const randomIndex = Math.floor(Math.random() * articles.length);
+  randomIndex = Math.floor(Math.random() * articles.length);
   const randomArticle = articles[randomIndex];
+
+  readFlag.innerHTML = '🔲';
+  if (randomArticle.isRead) readFlag.innerHTML = '✅';
+
   articleTitle.href = randomArticle.url;
   articleTitle.innerHTML = randomArticle.title;
+  readFlag.style.opacity = 1;
 };
+
+const onClickChangeReadFlag = () => {
+  articles[randomIndex].isRead = true;
+  readFlag.innerHTML = '✅';
+  console.log(articles);
+};
+
 btn.addEventListener('click', onClickMovetoRandomPage);
+readFlag.addEventListener('click', onClickChangeReadFlag);
