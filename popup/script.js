9e88c3850360af5d715e17a736a2fae73cf90bc3
Bible <bearsk711@gmail.com>
2023-04-27 22:07:26 +0900

    Show the title of selected article

--- popup/script.js
+++ popup/script.js
@@ -1,7 +1,10 @@
 const btn = document.querySelector('.randomBtn');
+const articleTitle = document.querySelector('.articleTitle');
 
 const onClickMovetoRandomPage = () => {
   const randomIndex = Math.floor(Math.random() * articles.length);
-  btn.href = articles[randomIndex].url;
+  const randomArticle = articles[randomIndex];
+  articleTitle.href = randomArticle.url;
+  articleTitle.innerHTML = randomArticle.title;
 };
 btn.addEventListener('click', onClickMovetoRandomPage);
