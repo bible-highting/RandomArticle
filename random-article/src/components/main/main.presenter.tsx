f82a27e19a8478371de7768e2c0cf562b7b5429c
Bible <bearsk711@gmail.com>
2023-05-23 17:24:30 +0900

    basic markup

--- random-article/src/components/main/main.presenter.tsx
+++ random-article/src/components/main/main.presenter.tsx
@@ -3,9 +3,12 @@ import * as s from './main.style';
 export default function MainPageUI() {
   return (
     <>
-      <s.MainTitle>
-        <h1>Random Article</h1>
-      </s.MainTitle>
+      <s.Wrapper>
+        <s.Title>Random Article</s.Title>
+        <s.RandomBtn>기사 생성</s.RandomBtn>
+        <s.Article>제목</s.Article>
+        <s.IsRead>읽음</s.IsRead>
+      </s.Wrapper>
     </>
   );
 }
