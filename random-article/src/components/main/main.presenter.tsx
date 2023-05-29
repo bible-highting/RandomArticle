3ba0fb33c886072b77433420498becff82423c45
Bible <bearsk711@gmail.com>
2023-05-29 17:11:57 +0900

    초기화면 카드 숨김

--- random-article/src/components/main/main.presenter.tsx
+++ random-article/src/components/main/main.presenter.tsx
@@ -17,33 +17,37 @@ export default function MainPageUI(props: IPropsMainPageUI) {
           <s.RandomBtn type='primary' onClick={props.clickCreateRandomArticle}>
             기사 생성
           </s.RandomBtn>
-          <a href={props.selectedArticle.link} target='_blank'>
-            {/* TODO - 추후 a없애고 onClickMotoToArticle로 구현? js로 페이지 이동하는 법 알기 router면 되려나*/}
-            <s.Article
-              hoverable
-              style={{ width: 240 }}
-              cover={
-                <img
-                  alt='example'
-                  src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
-                  height={150}
-                  style={{ objectFit: 'cover' }}
-                />
-              }
-            >
-              <Meta
-                title={props.selectedArticle.title}
-                description={props.selectedArticle.link}
-                style={{ textAlign: 'left' }}
-              />
-            </s.Article>
-          </a>
-          <s.IsRead
-            checked={props.selectedArticle.isRead}
-            onChange={props.onChangeIsRead}
-          >
-            Done
-          </s.IsRead>
+          {props.selectedArticle.id && (
+            <>
+              <a href={props.selectedArticle.link} target='_blank'>
+                {/* TODO - 추후 a없애고 onClickMotoToArticle로 구현? js로 페이지 이동하는 법 알기 router면 되려나*/}
+                <s.Article
+                  hoverable
+                  style={{ width: 240 }}
+                  cover={
+                    <img
+                      alt='example'
+                      src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+                      height={150}
+                      style={{ objectFit: 'cover' }}
+                    />
+                  }
+                >
+                  <Meta
+                    title={props.selectedArticle.title}
+                    description={props.selectedArticle.link}
+                    style={{ textAlign: 'left' }}
+                  />
+                </s.Article>
+              </a>
+              <s.IsRead
+                checked={props.selectedArticle.isRead}
+                onChange={props.onChangeIsRead}
+              >
+                Done
+              </s.IsRead>
+            </>
+          )}
         </s.BoxWrapper>
       </s.Wrapper>
     </>
